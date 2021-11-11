c1eb27e7324c87896b2775a92219c4b0f0a7e227
sofiagermer <sofiagermer8@gmail.com>
2021-11-11 14:29:13 +0000

    1.2 Done

--- Fib.hs
+++ Fib.hs
@@ -3,4 +3,13 @@ fibRec :: (Integral a) => a -> a
 
 fibRec n = if n <= 1 then n
            else fibRec (n-1) + fibRec(n-2)
-            
+
+--EXERCÍCIO 1.2
+fibLista :: (Integral a) => a -> a
+
+fibLista x = fibListaAux[x]
+fibListaAux[0] = 0;
+fibListaAux[1] = 1;
+fibListaAux[x] = fibListaAux[x-1] + fibListaAux[x-2] 
+
+
