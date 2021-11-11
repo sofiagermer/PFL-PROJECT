df6db93f5a9fac595b9faffafba6e0da9d79fc0a
sofiagermer <sofiagermer8@gmail.com>
2021-11-11 14:57:36 +0000

    1.3 Done

--- Fib.hs
+++ Fib.hs
@@ -7,9 +7,15 @@ fibRec n = if n <= 1 then n
 --EXERCÍCIO 1.2
 fibLista :: (Integral a) => a -> a
 
-fibLista x = fibListaAux[x]
+fibLista n = fibListaAux[n]
 fibListaAux[0] = 0;
 fibListaAux[1] = 1;
-fibListaAux[x] = fibListaAux[x-1] + fibListaAux[x-2] 
+fibListaAux[n] = fibListaAux[n-1] + fibListaAux[n-2] 
 
+-- EXERCÍCIO 1.3
+fibListaInfinitaAux :: [Integer]
+fibListaInfinitaAux = 0 : 1 : [a+b | (a,b)<-zip fibListaInfinitaAux (tail fibListaInfinitaAux)]
+
+fibListaInfinita :: Int -> Integer
+fibListaInfinita n = fibListaInfinitaAux!! n
 
